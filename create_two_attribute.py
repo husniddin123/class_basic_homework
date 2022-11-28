d164204a688b69bc536b4ec03d4a02bf4e3d8450
husniddin <baxodirovhusniddin66@gemal.com>
2022-11-28 18:32:47 +0500

    hjhfgjtykjeb

--- create_two_attribute.py
+++ create_two_attribute.py
@@ -3,9 +3,10 @@
 
 #that has a name("name") and a age("age")
 class Person:
-    def __init__(self,age,name) -> None:
+    def __init__(self, name, age) -> None:
         self.age = age
         self.name = name
 
-person1= Person (23,'azim')   
+person1= Person('ali',23)   
+
     
